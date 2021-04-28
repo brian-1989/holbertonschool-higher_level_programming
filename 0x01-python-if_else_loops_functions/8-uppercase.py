1875c690a32db6358d7bb42ba63d7a451b70eae2
Brian Zapata <dzapata397@gmail.com>
2021-04-28 03:39:54 +0000

    Point_8 Commit-3

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,10 +2,10 @@
 def uppercase(str):
     for i in str:
         j = ord(i)
-        # condiction to check if j is: uppercase, space or number
-        if j >= 65 and j <= 90 or j == 32 or j >= 48 and j <= 57:
-            j = ord(i)
-        else:
+        # condiction to check if j is lower
+        if j >= 97 and j <= 122:
             j = ord(i) - 32
+        else:
+            j = ord(i)
         print("{}".format(chr(j)), end="")
     print(end="\n")
