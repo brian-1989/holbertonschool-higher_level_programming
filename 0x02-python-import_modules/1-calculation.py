a807e9f6abb2951a8ac885b7450958e562936915
Brian Zapata <dzapata397@gmail.com>
2021-04-29 03:05:25 +0000

    Point_1 Commit-7

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    # import the calculator file, to call all the functions
     from calculator_1 import add, sub, mul, div
     a = 10
     b = 5
-    # print the result of each function
     print("{} + {} = {}".format(a, b, add(a, b)))
     print("{} - {} = {}".format(a, b, sub(a, b)))
     print("{} * {} = {}".format(a, b, mul(a, b)))
