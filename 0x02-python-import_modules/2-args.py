754b28911594ed732d694d292f5a34125c91c8c6
Brian Zapata <dzapata397@gmail.com>
2021-04-29 04:18:35 +0000

    point_2 Commit-3

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,7 +3,10 @@ if __name__ == "__main__":
     import sys
     argv = len(sys.argv) - 1
     if argv == 0:
-        print("0 arguments")
+        print("0 arguments.")
+    elif argv == 1:
+        print("{} argument:".format(argv))
+        print("{}: {}".format(argv, sys.argv[1]))
     else:
         print("{} arguments:".format(argv))
         for i in range(0, argv):
