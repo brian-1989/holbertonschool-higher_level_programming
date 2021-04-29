f46f5a5526ca521431f69cd5e5540fdde276925f
Brian Zapata <dzapata397@gmail.com>
2021-04-29 04:10:19 +0000

    point_2 Commit

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    argv = len(sys.argv) - 1
+    if argv == 0:
+        print("0 arguments")
+    else:
+        print("{} arguments:".format(argv))
+        for i in range(0, argv):
+            i = i + 1
+            print("{} {}".format(i, sys.argv[i]))
