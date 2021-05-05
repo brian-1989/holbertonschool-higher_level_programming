1eca38bc0c64338861191dba62429c5845c07575
Brian Zapata <dzapata397@gmail.com>
2021-05-05 23:42:01 +0000

    Point_1 Commit-4

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -3,7 +3,7 @@ def element_at(my_list, idx):
     _len = len(my_list)
     if idx < 0:
         return None
-    elif idx > _len:
+    elif idx >= _len:
         return None
     elif idx == 0 and _len == 0:
         return None
