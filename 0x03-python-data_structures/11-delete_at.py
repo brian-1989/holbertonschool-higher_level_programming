d99b5129ebd21f3344c95f690821f83977312796
Brian Zapata <dzapata397@gmail.com>
2021-05-06 23:53:46 +0000

    Point_11 Commit-2

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -3,7 +3,7 @@ def delete_at(my_list=[], idx=0):
     if idx < 0 or idx > len(my_list):
         return my_list
     else:
-        for i in my_list:
+        for i in range(0, len(my_list)):
             if i == idx:
                 my_list.remove(my_list[i])
     return my_list
