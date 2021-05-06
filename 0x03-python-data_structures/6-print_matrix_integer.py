df8190bbd1b281ce8b86c968940e952794142d67
Brian Zapata <dzapata397@gmail.com>
2021-05-06 16:46:35 +0000

    Point_6 Commit-5

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
+    # travel the elements of the matrix
     for i in range(0, len(matrix)):
+        # condicition to when the matrix is empty
         if matrix == [[]]:
             print(end="\n")
             break
+        # travel the positions that have each element
         for j in range(0, len(matrix[i])):
+            # condicition to remove the space at final of each element
             if j == len(matrix[i]) - 1:
                 print("{:d}".format(matrix[i][j]), end="")
             else:
