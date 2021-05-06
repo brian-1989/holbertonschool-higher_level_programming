39710374ce990032c6ed1ae2c84feb824cb91508
Brian Zapata <dzapata397@gmail.com>
2021-05-06 16:07:06 +0000

    Point_6 Commit

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    _len = len(matrix)
+    for i in range(0, _len):
+        if _len == 1:
+            print(end="\n")
+            break
+        for j in range(0, _len):
+            print("{}".format(matrix[i][j]), end="")
+        print(end="\n")
