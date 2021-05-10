aaf9543f9757984a58285939bb567272c84961c0
Brian Zapata <dzapata397@gmail.com>
2021-05-10 03:36:02 +0000

    Point_10 Commit

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary is None:
+        return None
+    # take out the values of the dictionary
+    values = a_dictionary.values()
+    # enter the dictionary values on a list
+    list_values = list(values)
+    list_values.sort()
+    # maximum dictionary value
+    max_value = list_values[-1]
+    for i, j in a_dictionary.items():
+        # condition to compare values with the maximum dictionary value
+        if j == max_value:
+            return i
+    return None
