112e71edf25130bebea7904e7def082ce33477ff
Brian Zapata <dzapata397@gmail.com>
2021-05-10 02:48:09 +0000

    Point_8 Commit

---
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def simple_delete(a_dictionary, key=""):
+    if key not in a_dictionary:
+        pass
+    else:
+        # (del), this condition to delete elemento of dictionary
+        del a_dictionary[key]
+    return a_dictionary
