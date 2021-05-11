1d7683c48088d16e6299f7cf2da0dee17e1a023c
Brian Zapata <dzapata397@gmail.com>
2021-05-11 03:27:03 +0000

    Point_12 Commit-3

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if roman_string == "" or roman_string is None:
+    if not isinstance(roman_string, str) or roman_string is None:
         return 0
     sum = 0
     my_list = []
