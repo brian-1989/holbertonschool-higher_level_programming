e18c7fbfe0fd4ee14bcf5a4b28ea871f20daef41
Brian Zapata <dzapata397@gmail.com>
2021-05-17 18:35:15 +0000

    Point_0 whit comments Commit

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
+    # travel the list with the range of argument (x)
     for j in range(0, x):
         try:
+            # with this condition we make the exception
             if my_list[j] == x:
                 print("{}".format(my_list[j]))
                 return my_list[j]
             else:
                 print("{}".format(my_list[j]), end="")
+        # exception for when occurrence the error
         except IndexError:
             print(end="\n")
             return my_list[-1]
