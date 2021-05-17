36de0b809ad30d2ae82622db4e45188815e09d80
Brian Zapata <dzapata397@gmail.com>
2021-05-17 20:58:16 +0000

    Point_3 Commit

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    try:
+        _div = a / b
+    except ZeroDivisionError:
+        # It is assigned to the variable _div, the value of none
+        _div = None
+    print("Inside result: {}".format(_div))
+    return _div
