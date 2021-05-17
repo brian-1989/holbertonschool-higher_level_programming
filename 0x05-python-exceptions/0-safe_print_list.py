6401018ec48fa10f0110746e83d4f03d608d2834
Brian Zapata <dzapata397@gmail.com>
2021-05-17 18:59:20 +0000

    Point_0 The return is fixed Commit

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
+    num_count = 0
     # travel the list with the range of argument (x)
     for j in range(0, x):
         try:
             # with this condition we make the exception
             if my_list[j] == x:
                 print("{}".format(my_list[j]))
-                return my_list[j]
+                num_count += 1
+                return num_count
             else:
                 print("{}".format(my_list[j]), end="")
+                num_count += 1
         # exception for when occurrence the error
         except IndexError:
-            pass
-    print(end="\n")
-    return my_list[-1]
+            print(end="\n")
+            return num_count
