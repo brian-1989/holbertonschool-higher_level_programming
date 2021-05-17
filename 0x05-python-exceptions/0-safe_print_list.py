376501342d171c427f5cc8819a70534b576d77b1
Brian Zapata <dzapata397@gmail.com>
2021-05-17 19:03:51 +0000

    Point_0 is removed if else Commit

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,14 +5,10 @@ def safe_print_list(my_list=[], x=0):
     for j in range(0, x):
         try:
             # with this condition we make the exception
-            if my_list[j] == x:
-                print("{}".format(my_list[j]))
-                num_count += 1
-                return num_count
-            else:
-                print("{}".format(my_list[j]), end="")
-                num_count += 1
+            print("{}".format(my_list[j]))
+            num_count += 1
         # exception for when occurrence the error
         except IndexError:
-            print(end="\n")
-            return num_count
+            pass
+    print(end="\n")
+    return num_count
