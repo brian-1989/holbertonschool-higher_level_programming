ab296989a4baa7dd8157fc74defb9da4d398fbb8
Brian Zapata <dzapata397@gmail.com>
2021-05-19 01:04:46 +0000

    Point_1 Commit

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" define class square"""
+
+
+class Square:
+    """ Private instance attribute """
+
+    def __init__(self, size):
+        """ Args:
+        param1 (int): The first parameter. """
+
+        self.__size = size
