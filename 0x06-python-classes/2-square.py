ea4a281804135074f4ba888e2cf6b017b861e17c
Brian Zapata <dzapata397@gmail.com>
2021-05-19 02:48:49 +0000

    Point_2 Commit

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" define class square"""
+
+
+class Square:
+    """ Private instance attribute """
+
+    def __init__(self, size=0):
+        """ Args:
+        size: The first parameter. """
+
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif int(size) < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
