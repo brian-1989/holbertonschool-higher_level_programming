a98e309c0e5bae4c6b99d5882836e297ce7c1ab5
Brian Zapata <dzapata397@gmail.com>
2021-05-19 05:23:26 +0000

    Point_4 Commit

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+""" define class square"""
+
+
+class Square:
+    """ Private instance attribute """
+
+    def __init__(self, size=0):
+        """ Args:
+        size: The first parameter. """
+
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif int(size) < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+    """ Function to calculate the area of a square """
+
+    def area(self):
+        self.area = self.__size**2
+        """ Returns:
+        the area of a square. """
+        return self.area
+    """ Function to calculate the area of a square """
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif int(value) < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
