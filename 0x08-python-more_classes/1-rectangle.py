3781ed2ec0964eca4758cfc8b497c2d643667abf
Brian Zapata <dzapata397@gmail.com>
2021-05-21 02:07:20 +0000

    Point_1 Errors also put on __init__ Commit

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -12,8 +12,16 @@ class Rectangle:
         height: heigth of rectangle.
 
         """
-        self.__width = width
-        self.__height = height
+        if type(width) is not int:
+            raise TypeError("width must be an integer")
+        elif width < 0:
+            raise ValueError("width must be >= 0")
+        elif type(height) is not int:
+            raise TypeError("height must be an integer")
+        elif height < 0:
+            raise ValueError("height must be >= 0")
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
