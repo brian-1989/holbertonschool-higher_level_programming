9b942ae11f4233f8e9ba145e0cfa9ab7079d32f3
Brian Zapata <dzapata397@gmail.com>
2021-05-25 16:28:09 +0000

    Functions to print a square created Commit

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Square
+"""
+
+
+def print_square(size):
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    if not isinstance(size, float) and size < 0:
+        raise TypeError("size must be an integer")
+    for i in range(0, size):
+        for j in range(0, size):
+            print("#", end="")
+        if i < size:
+            print(end="\n")
