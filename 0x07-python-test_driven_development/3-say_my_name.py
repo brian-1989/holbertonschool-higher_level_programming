4f8b00af910c543140c8c4c786392911e8d52600
Brian Zapata <dzapata397@gmail.com>
2021-05-25 05:03:58 +0000

    Function say_my_name created Commit

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+My name and my last name
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    """Function that print the name and the last name.
+
+    """
+    if type(first_name) is not str:
+        raise TypeError("first_name must be a string")
+    elif type(last_name) is not str:
+        raise TypeError("last_name must be a string")
+    print("My name is {} {}".format(first_name, last_name))
