a023a6948cdf368338f408cd98909c628aecafc8
Brian Zapata <dzapata397@gmail.com>
2021-05-26 00:25:48 +0000

    fixed condition and fixed comments Commit

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -6,14 +6,18 @@ Sum
 
 def add_integer(a, b=98):
     """Function that sum two number.
-    Return the total sum of two number.
+    Args:
+        a = first value.
+        b = second value.
+    Return the total sum of two value.
 
     """
-    if not isinstance(a, int) and not isinstance(a, float):
+    if type(a) is not int and type(a) is not float:
         raise TypeError("a must be an integer")
-    if not isinstance(b, int) and not isinstance(b, float):
+    if type(b) is not int and type(b) is not float:
         raise TypeError("b must be an integer")
-    if type(a) is float or type(b) is float:
+    if type(a) is float:
         a = int(a)
+    if type(b) is float:
         b = int(b)
     return a + b
