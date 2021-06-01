547908a0e81ec1f7062979ea1f66e15cfc0ca71c
Brian Zapata <dzapata397@gmail.com>
2021-06-01 23:55:40 +0000

    Created the method ared Commit

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+base geometry
+"""
+
+
+class BaseGeometry:
+    """class base geometry.
+
+    """
+    def area(self):
+        """method that raise an exception.
+
+        """
+        raise Exception("area() is not implemented")
