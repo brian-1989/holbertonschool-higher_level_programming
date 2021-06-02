00c6e53730e14883030a2da7093910bec987ed5c
Brian Zapata <dzapata397@gmail.com>
2021-06-02 23:57:50 +0000

    Encoding utf-8 Commit

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -8,7 +8,7 @@ def read_file(filename=""):
     """function that read the text of a file.
 
     """
-    with open(filename, 'r') as text:
+    with open(filename, 'r', encoding="utf-8") as text:
         read_text = text.read()
-    print("{}".format(read_text))
-    text.close()
+        print("{}".format(read_text))
+        text.close()
