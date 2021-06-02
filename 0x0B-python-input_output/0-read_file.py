464d2df214f077811b9cffea184bf966aff360e6
Brian Zapata <dzapata397@gmail.com>
2021-06-02 21:43:19 +0000

    Created the function read file Commit

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Read file.
+"""
+
+
+def read_file(filename=""):
+    """function that read the text of a file.
+
+    """
+    with open(filename, 'r') as text:
+        read_text = text.read()
+        print(read_text)
