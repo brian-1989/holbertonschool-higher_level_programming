ba2bc3516d7311ff7bc01413d4616e6c933ef5f4
Brian Zapata <dzapata397@gmail.com>
2021-06-02 01:48:33 +0000

    The basegeometry class took off Commit

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -3,29 +3,7 @@
 base geometry
 """
 
-
-class BaseGeometry:
-    """class base geometry.
-
-    """
-    def area(self):
-        """method that raise an exception.
-
-        """
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """method that validates the argumnets value and created
-        an exception.
-        Args:
-            name: name of user
-            value: value to validates
-
-        """
-        if type(value) is not int:
-            raise TypeError("{} must be an integer".format(name))
-        if value is 0 or value < 0:
-            raise ValueError("{} must be greater than 0".format(name))
+BaseGeometry = __import__('7-base_geometry').BaseException
 
 
 class Rectangle(BaseGeometry):
