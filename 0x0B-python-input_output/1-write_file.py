0b12b5a5fb0218617f744f894e8c3ae9ca561e73
Brian Zapata <dzapata397@gmail.com>
2021-06-02 22:19:44 +0000

    Created the function write file Commit

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Write in a file.
+"""
+
+
+def write_file(filename="", text=""):
+    """function that writes a text in a file and if the file
+    does not exist, it creates it.
+    Args:
+        filename: name of file
+        text: text a write in the file
+    Return the character number.
+
+    """
+    if not filename:
+        with open(filename, 'w +') as _text:
+            write_text = _text.write(text)
+            _text.close()
+            return write_text
+    else:
+        with open(filename, 'a') as _text:
+            write_text = _text.write(text)
+            _text.close()
+            return write_text
