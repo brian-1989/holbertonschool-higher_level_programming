6c15433d0917f3165a3785698325be7b3bee2335
Brian Zapata <dzapata397@gmail.com>
2021-06-02 02:14:49 +0000

    Created the method area and str Commit

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+base geometry
+"""
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """class receiving the inheritance BaseGeometry.
+
+    """
+    def __init__(self, width, height):
+        """initial method that receiving the arguments.
+        Agrgs:
+            width: width of Rectangle.
+            height: height of Rectangle.
+
+        """
+        self.__width = width
+        self.__height = height
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+
+    def __str__(self) -> str:
+        """method that return the description of a rectanlge.
+
+        """
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
+
+    def area(self):
+        """method that get the area of a rectangle.
+        Return the area of rectangle.
+
+        """
+        return self.__width * self.__height
