7da7049a9762915af489a1e2d8686f37a591503f
Brian Zapata <dzapata397@gmail.com>
2021-06-02 03:24:30 +0000

    Created the method str Commit

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+Square
+"""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """class square that inherits.
+
+    """
+    def __init__(self, size):
+        """initial method that take the size of square.
+        Agrs:
+            size: size of square.
+
+        """
+        super().__init__(size, size)
+        self.__size = size
+        self.__str__()
+
+    def __str__(self):
+        """method that return the description of a square.
+
+        """
+        return "[Square] {}/{}".format(self.__size, self.__size)
+
+    def area(sefl):
+        """method that that get the area of a square.
+        Return the area of a square.
+
+        """
+        return sefl.__size**2
