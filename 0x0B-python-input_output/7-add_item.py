d97854bfaee91f7051317e4758f1eadcc93692c3
Brian Zapata <dzapata397@gmail.com>
2021-06-03 06:31:23 +0000

    Created script that add elments to a list Commit

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Script that adds all arguments to a Python list.
+"""
+
+
+from sys import argv
+
+
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+try:
+    my_list = load_from_json_file("add_item.json")
+except:
+    my_list = list()
+for i in range(1, len(argv)):
+    my_list.append(argv[i])
+save_to_json_file(my_list, "add_item.json")
