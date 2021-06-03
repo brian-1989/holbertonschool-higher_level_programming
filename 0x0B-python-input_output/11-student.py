b6492584cf0a4cd37f6db826d4c5df2629a9a895
Brian Zapata <dzapata397@gmail.com>
2021-06-03 19:34:49 +0000

    Created the method reload from json Commit

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""
+Class Student.
+"""
+
+
+class Student:
+    """class that defines a student.
+
+    """
+    def __init__(self, first_name, last_name, age):
+        """initial method that take the arguments.
+        Args:
+             first_name: name of student
+             last_name: last name of student
+             age: age of student
+
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """method that adds in a dictionary existing attributes.
+        Return the dictionary with the attributes.
+
+        """
+        my_dic = {}
+        if type(attrs) is list:
+            for i in attrs:
+                if i is 'first_name':
+                    my_dic[i] = self.first_name
+                if i is 'last_name':
+                    my_dic[i] = self.last_name
+                if i is 'age':
+                    my_dic[i] = self.age
+            return my_dic
+        else:
+            return self.__dict__
+
+    def reload_from_json(self, json):
+        """method that update all attributes of the Student.
+
+        """
+        for key, value in json.items():
+            setattr(self, key, value)
