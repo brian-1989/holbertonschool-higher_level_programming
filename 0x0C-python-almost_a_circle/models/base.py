b11cbf9182a43c88464f351220734e296d2e5ce9
Brian Zapata <dzapata397@gmail.com>
2021-06-05 19:03:26 +0000

    Created the classmethod save to file Commit

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -5,6 +5,7 @@ Base.
 
 
 import json
+from os import path
 
 
 class Base:
@@ -19,12 +20,13 @@ class Base:
             id: integer.
 
         """
-        if id is not None:
+        if id is None:
             self.id = id
         else:
             Base.__nb_objects += 1
-            self.id = self.__nb_objects
+            self.id = self.__nb_object
 
+    @staticmethod
     def to_json_string(list_dictionaries):
         """method that converts a dictionary in a string.
         Return the JSON string representation of list_dictionaries.
@@ -33,3 +35,29 @@ class Base:
         if list_dictionaries is None or list_dictionaries is []:
             return []
         return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """method that write a un file the JSON string representation
+        of list_objs.
+
+        """
+        my_list = []
+        for i in list_objs:
+            my_list.append(i.__dict__)
+        if path.exists("{}.json".format(cls.__name__)):
+            with open("{}.json".format(cls.__name__), 'a') as text:
+                string_list = Base.to_json_string(my_list)
+                if list_objs is None:
+                    text.write("[]")
+                else:
+                    text.write(string_list)
+                text.close()
+        else:
+            with open("{}.json".format(cls.__name__), 'w') as text:
+                string_list = Base.to_json_string(my_list)
+                if list_objs is None:
+                    text.write("[]")
+                else:
+                    text.write(string_list)
+                text.close()
