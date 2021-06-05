fcd060c83f51cc310cae6153387cfeb1ca915df4
Brian Zapata <dzapata397@gmail.com>
2021-06-05 16:53:49 +0000

    Created the method to json string Commit

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -4,6 +4,9 @@ Base.
 """
 
 
+import json
+
+
 class Base:
     """class base to the proyect.
 
@@ -21,3 +24,12 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = self.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        """method that converts a dictionary in a string.
+        Return the JSON string representation of list_dictionaries.
+
+        """
+        if list_dictionaries is None or list_dictionaries is []:
+            return []
+        return json.dumps(list_dictionaries)
