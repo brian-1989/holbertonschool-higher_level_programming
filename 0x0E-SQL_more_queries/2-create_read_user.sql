86a3b2c40b985a1a5234cc39e651f93f6a303376
Brian Zapata <dzapata397@gmail.com>
2021-07-14 02:46:28 +0000

    Add the 't' character, at word no

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,6 +1,6 @@
 -- This scrip creates the database hbtn_0d_2 and the user hbtn_0d_2
 -- with only permission of SELECT in the database hbtn_0d_2
-CREATE DATABASE IF NO EXISTS hbtn_0d_2;
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd';
 GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost;
 FLUSH PRIVILEGES;
