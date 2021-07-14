196886fe02108f2f94a6d281db4ce7feb5f2d55c
Brian Zapata <dzapata397@gmail.com>
2021-07-14 03:47:15 +0000

    creates the database hbtn_0d_usa and the table cities

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,10 @@
+-- This scrip creates the database hbtn_0d_usa and the table cities
+-- The id column INT unique, auto generated, can’t be null and is a primary key
+-- The state_id column INT, can’t be null and must be a FOREIGN KEY that
+-- references to id of the states table
+-- The name column VARCHAR(256) can’t be null
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities (id INT UNIQUE AUTO_INCREMENTE NOT NULL PRIMARY KEY,
+state_id INT NOT NULL FOREIGN KEY(id) REFERENCES states(id),
+name VARCHAR(256) IS NOT NULL);
