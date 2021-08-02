fdd038282c1378f46682c73c81d82db747f9e503
Brian Zapata <dzapata397@gmail.com>
2021-08-02 17:18:33 -0500

    semistandard was organized

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 /* This script prints a message depending of the number of arguments passed */
-if (process.argv[3]){
-    console.log('Arguments found');
-} else if (process.argv[2]){
-    console.log('Argument found');
+if (process.argv[3]) {
+  console.log('Arguments found');
+} else if (process.argv[2]) {
+  console.log('Argument found');
 } else {
-    console.log('No argument');
+  console.log('No argument');
 }
