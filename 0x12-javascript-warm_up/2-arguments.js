8c65fea57286cb5503046bac8e1c557b6b21ce10
Brian Zapata <dzapata397@gmail.com>
2021-08-02 17:10:24 -0500

    The conditions to the arguments passed was created

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+/* This script prints a message depending of the number of arguments passed */
+if (process.argv[3]){
+    console.log('Arguments found');
+} else if (process.argv[2]){
+    console.log('Argument found');
+} else {
+    console.log('No argument');
+}
