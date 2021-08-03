bf2b5b39c7ef988542e69f36905c5993d54abb90
Brian Zapata <dzapata397@gmail.com>
2021-08-02 19:02:06 -0500

    The loop for to print the array, was created

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+/* This script prints 3 linesbut by using an array of string and a loop */
+const myArray = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (let i = 0; myArray[i]; i++) {
+  console.log(myArray[i]);
+}
