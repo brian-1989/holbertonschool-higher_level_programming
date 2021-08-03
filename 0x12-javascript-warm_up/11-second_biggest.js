d2b2531a1f008e8928bb3ae6667b92bb6ba719c5
Brian Zapata <dzapata397@gmail.com>
2021-08-02 23:57:39 -0500

    The condition was organized

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,13 +1,14 @@
 #!/usr/bin/node
 /* This script that searches the second biggest integer in the list of arguments. */
 const myArray = [];
-if (isNaN(parseInt(process.argv[2])) || parseInt(process.argv[2]) === 1) {
+let lengthMyArray = process.argv.length;
+if (isNaN(parseInt(process.argv[2])) || lengthMyArray === 3) {
   console.log(0);
 } else {
   for (let i = 2; process.argv[i]; i++) {
     myArray.push(process.argv[i]);
   }
   myArray.sort(function (a, b) { return a - b; });
-  const lengthMyArray = myArray.length;
+  lengthMyArray = myArray.length;
   console.log(myArray[lengthMyArray - 2]);
 }
