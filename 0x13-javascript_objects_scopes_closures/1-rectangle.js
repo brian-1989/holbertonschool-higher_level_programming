9dd4b4aa664cd94cf242c59d2b3ac83d87874524
Brian Zapata <dzapata397@gmail.com>
2021-08-03 16:11:50 -0500

    The Rectangle class and the constructor of the class, was created

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+/* This script define an class called rectangle and create a
+constructor, with 2 arguments and Initialize the instance attributes */
+class Rectangle {
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
+}
+module.exports = Rectangle;
