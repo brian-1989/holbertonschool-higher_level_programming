6597759a3a8a28b9d47e9d9bf73f29426828ccfc
Brian Zapata <dzapata397@gmail.com>
2021-08-04 12:24:33 -0500

    The conditions to crate, read and write a file, was created

---
+++ 0x12-javascript-warm_up/102-concat.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+/* This script that concats 2 files. */
+let fileOpen1 = process.argv[2];
+let fileOPen2 = process.argv[3];
+let fileCreate = process.argv[4];
+const fs = require('fs');
+fs.readFile(fileOpen1, 'utf-8', (error1, file1) => {
+  fs.readFile(fileOPen2, 'utf-8', (error2, file2) => {
+    fs.writeFile(fileCreate, file1 + file2, () => {});});});
