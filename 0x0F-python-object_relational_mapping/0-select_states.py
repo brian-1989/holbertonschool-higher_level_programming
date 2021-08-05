78b0c627e7cc008f008d7e4e413783f93d4a9db7
Brian Zapata <dzapata397@gmail.com>
2021-08-05 17:16:16 -0500

    The function to connect the file .py and the data base, and print a query was created

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+import MySQLdb
+from sys import argv
+
+def data_base():
+    """ Function to connect a file python with an data base.
+
+    """
+    my_dd = MySQLdb.connect(host="localhost", user=argv[1], passwd="Brian1995#", db=argv[3], port=3306)
+    my_cursor = my_dd.cursor()
+    my_cursor.execute("SELECT id, name FROM states ORDER BY id ASC")
+    data = my_cursor.fetchone()
+    while data:
+        print (data)
+        data = my_cursor.fetchone()
+    my_dd.close()
+
+if __name__ == '__main__':
+    """ When it imported the file not be executed.
+
+    """
+    data_base()
