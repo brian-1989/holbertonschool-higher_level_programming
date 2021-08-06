07a115e43cb8285baa494127cf7ab21925558202
Brian Zapata <dzapata397@gmail.com>
2021-08-06 17:21:00 -0500

    The engine, the session and the query to changes the name of a State object from the database hbtn_0e_6_usa, was created

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" This script that changes the name of a State object
+from the database hbtn_0e_6_usa.
+
+"""
+
+
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm.session import Session
+from model_state import Base, State
+import sys
+
+if __name__ == '__main__':
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format
+                           (sys.argv[1],
+                            sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    session = Session(bind=engine)
+    a = session.query(State).get(2)
+    # Update the name table with the id = 2
+    a.name = 'New Mexico'
+    session.add(a)
+    session.commit()
+    session.close()
