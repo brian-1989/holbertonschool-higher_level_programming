87d600d01bae2aadf4f96d81f2725d878932a540
Brian Zapata <dzapata397@gmail.com>
2021-08-05 21:00:19 -0500

    Add prevention at part of query to avoid the sql injection

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+""" Connect a file .py with an data base.
+
+"""
+
+
+import MySQLdb
+from sys import argv
+
+
+def data_base():
+    """ Function to connect a file python with an data base and print an query.
+
+    """
+    my_db = MySQLdb.connect(user=argv[1],
+                            passwd="Brian1995#",
+                            db=argv[3])
+    my_cursor = my_db.cursor()
+    my_cursor.execute("SELECT id, name FROM states WHERE name=%s", (argv[4],))
+    data = my_cursor.fetchone()
+    while data:
+        print(data)
+        data = my_cursor.fetchone()
+    my_db.close()
+
+
+if __name__ == '__main__':
+    """ When it imported the file not be executed.
+
+    """
+    data_base()
