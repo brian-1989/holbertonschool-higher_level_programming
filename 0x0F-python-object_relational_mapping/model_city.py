d4ff5ff58b3809d26bb19a0ddb567bfbcde8f3cf
Brian Zapata <dzapata397@gmail.com>
2021-08-06 20:40:25 -0500

    The City class, the cities table, the id, name and state_id columns and the state varible to relate that variable, with the states table, was created

---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" City class.
+"""
+
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy.sql.schema import ForeignKey
+from model_state import State
+
+Base = declarative_base()
+
+
+class City(Base):
+    """ This class inherits of the Base class and serve to connect
+    the states and cities tables.
+
+    """
+    __tablename__ = 'cities'
+    id = Column(Integer, nullable=False, primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey(State.id))
+    # It is used to connect a variable with the states table
+    # and can access the columns of said table
+    state = relationship(State)
