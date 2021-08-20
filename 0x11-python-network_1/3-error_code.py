622f9d0a80cc3ae36ba8da1fbd6a9047f8d836d0
Brian Zapata <dzapata397@gmail.com>
2021-08-20 13:18:16 -0500

    The urllin.request and sys library was used to takes an URL displays the body of the response (decoded in utf-8)

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" This script takes in a URL, sends a request to the URL and
+displays the body of the response (decoded in utf-8).
+
+"""
+
+import urllib.request
+import sys
+
+if __name__ == "__main__":
+    _url = urllib.request.Request(sys.argv[1])
+    try:
+        with urllib.request.urlopen(_url) as response:
+            _page = response.read()
+    except(urllib.error.HTTPError) as _error:
+        print("Error code: {}".format(_error.code))
