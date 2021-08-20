86755e24a6b0a659e18e45b7068f2497ad384d82
Brian Zapata <dzapata397@gmail.com>
2021-08-20 12:12:21 -0500

    add :

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -8,6 +8,6 @@ if __name__ == "__main__":
     _url = urllib.request.Request('https://intranet.hbtn.io/status', )
     with urllib.request.urlopen(_url) as response:
         _page = response.read()
-        print("Body response")
+        print("Body response:")
         print("\t- type: {}\n\t- content: {}\n\t- utf8 content: {}".format(
             type(response.read()), _page, _page.decode('utf8')))
