1b57b635e8549957578e436b3df3214224e1a548
Brian Zapata <dzapata397@gmail.com>
2021-08-19 19:41:54 -0500

    The file with the complexity of your algorithm

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+
+def find_peak(list_of_integers):
+    """ Function that find a peak in a array.
+    arg:
+        lis_of_integers: array with the integers.
+
+    """
+    if list_of_integers == []:
+        return
+    peak_list = []
+    _len = len(list_of_integers) - 1
+    if list_of_integers[0] >= list_of_integers[1]:
+        peak_list.append(list_of_integers[0])
+    if list_of_integers[_len] >= list_of_integers[_len - 1]:
+        peak_list.append(list_of_integers[_len])
+    for i in range(2, _len):
+        if list_of_integers[i] >= list_of_integers[i - 1] and\
+                list_of_integers[i] >= list_of_integers[i + 1]:
+            peak = list_of_integers[i]
+            peak_list.append(peak)
+    if len(peak_list) == 1:
+        return peak_list[0]
+    else:
+        peak_list.sort()
+        return peak_list[-1]
