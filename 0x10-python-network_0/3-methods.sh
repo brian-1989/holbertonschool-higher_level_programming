97ebba8dc356742f7d6ab3c9dd4365e7bd4340d3
Brian Zapata <dzapata397@gmail.com>
2021-08-19 19:46:12 -0500

    add -

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script takes in a URL and displays all HTTP methods.
-curl -s -i -X allow "$1" | grep Allow: | cut -d " " -f 2
+curl -s -i -X allow "$1" | grep Allow: | cut -d " " -f 2-
