1afc3673d9fdd2b9971cc0a9a2dc667e0716e935
Brian Zapata <dzapata397@gmail.com>
2021-08-19 20:22:14 -0500

    simple

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -3,6 +3,7 @@
 
 """
 
+
 def find_peak(list_of_integers):
     """ Function that find a peak in a array.
     arg:
@@ -11,19 +12,5 @@ def find_peak(list_of_integers):
     """
     if list_of_integers == []:
         return
-    peak_list = []
-    _len = len(list_of_integers) - 1
-    if list_of_integers[0] >= list_of_integers[1]:
-        peak_list.append(list_of_integers[0])
-    if list_of_integers[_len] >= list_of_integers[_len - 1]:
-        peak_list.append(list_of_integers[_len])
-    for i in range(2, _len):
-        if list_of_integers[i] >= list_of_integers[i - 1] and\
-                list_of_integers[i] >= list_of_integers[i + 1]:
-            peak = list_of_integers[i]
-            peak_list.append(peak)
-    if len(peak_list) == 1:
-        return peak_list[0]
-    else:
-        peak_list.sort()
-        return peak_list[-1]
+    list_of_integers.sort()
+    return list_of_integers[-1]
