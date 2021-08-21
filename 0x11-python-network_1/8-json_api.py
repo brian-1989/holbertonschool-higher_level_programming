56634b465d2d6d4b5e0bf31f51c456eaa9642baa
Brian Zapata <dzapata397@gmail.com>
2021-08-20 21:31:14 -0500

    add response.raise_for_status

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -6,18 +6,22 @@ http://0.0.0.0:5000/search_user with the letter as a parameter.
 
 import requests
 import sys
+from requests.exceptions import HTTPError
 
 if __name__ == "__main__":
     if len(sys.argv) > 1:
         obj = {"q": sys.argv[1]}
     else:
         obj = {"q": ""}
-    response = requests.post('http://0.0.0.0:5000/search_user', data=obj)
-    _json = response.json()
     try:
+        response = requests.post('http://0.0.0.0:5000/search_user', data=obj)
+        response.raise_for_status()
+        _json = response.json()
         if len(_json) > 0:
             print("[{}] {}".format(_json['id'], _json['name']))
         else:
             print("No result")
-    except:
+    except(HTTPError) as error:
+        print("Not a valid JSON")
+    except Exception as err:
         print("Not a valid JSON")
