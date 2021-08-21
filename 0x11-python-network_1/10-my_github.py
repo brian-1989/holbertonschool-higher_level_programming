c5558f1877467a7d4101d08f7483c788dc5ddde9
Brian Zapata <dzapata397@gmail.com>
2021-08-20 21:13:25 -0500

    The requests and sys library was used to takes your GitHub credentials (username and password)

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" This script that takes your GitHub credentials (username and password).
+
+"""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    try:
+        response = requests.get(
+            'https://api.github.com/user', auth=(sys.argv[1], sys.argv[2]))
+        _json = response.json()
+        print(_json['id'])
+    except():
+        print(None)
