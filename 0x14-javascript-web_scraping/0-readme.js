4a7d32c665ad84fd347362189c745038cf9f20c7
Brian Zapata <dzapata397@gmail.com>
2021-09-14 12:45:27 -0500

    The jump line between the line 3 and line 5 was added

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,6 +1,7 @@
 #!/usr/bin/node
 /* This script print the content of a file */
 const fs = require('fs')
+
 fs.readFile(process.argv[2], 'utf-8', (err, data) => {
   if (err) {
     return console.error(err)
