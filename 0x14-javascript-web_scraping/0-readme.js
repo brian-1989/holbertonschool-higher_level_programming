f495c33fd30e27e944d6ead6949f06d4c1738d62
Brian Zapata <dzapata397@gmail.com>
2021-09-14 12:54:24 -0500

    The semicolon was added

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 /* This script print the content of a file */
-const fs = require('fs')
+const fs = require('fs');
 
 fs.readFile(process.argv[2], 'utf-8', (err, data) => {
   if (err) {
-    return console.error(err)
+    return console.error(err);
   }
-  console.log(data)
+  console.log(data);
 })
