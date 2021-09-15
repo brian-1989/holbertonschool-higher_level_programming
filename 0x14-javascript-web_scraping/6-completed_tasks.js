d3138428902b0a2f48a17a95ea4454345c45cef7
Brian Zapata <dzapata397@gmail.com>
2021-09-14 21:56:54 -0500

    The request.get was changed by request

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,13 +1,13 @@
 #!/usr/bin/node
 /* This script computes the number of tasks completed by user id */
 const request = require('request');
-const dict = {};
 
-request.get(process.argv[2], (error, process, body) => {
+request(process.argv[2], (error, process, body) => {
   if (error) {
     return console.error(error);
   }
   const listTask = JSON.parse(body);
+  const dict = {};
   for (let i = 1; i < 11; i++) {
     let countComplete = 0;
     for (const task of listTask) {
