1388857bcb368d0a2b6dcb11f3d91d739054991c
Brian Zapata <dzapata397@gmail.com>
2021-09-14 21:54:05 -0500

    The 'listTask' variable was added

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -7,9 +7,10 @@ request.get(process.argv[2], (error, process, body) => {
   if (error) {
     return console.error(error);
   }
+  const listTask = JSON.parse(body);
   for (let i = 1; i < 11; i++) {
     let countComplete = 0;
-    for (const task of JSON.parse(body)) {
+    for (const task of listTask) {
       if (task.userId === i && task.completed === true) {
         countComplete += 1;
       }
