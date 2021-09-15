a35e9f5044a1d2751d09cd7d809ff5c92ea45f6e
Brian Zapata <dzapata397@gmail.com>
2021-09-14 21:33:25 -0500

    The request to computes the number of tasks completed by user id and the save in a dictionary

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+/* This script computes the number of tasks completed by user id */
+const request = require('request');
+const dict = {};
+
+request.get(process.argv[2], (error, process, body) => {
+  if (error) {
+    return console.error(error);
+  }
+  for (let i = 1; i < 11; i++) {
+    let countComplete = 0;
+    for (const task of JSON.parse(body)) {
+      if (task.userId === i && task.completed === true) {
+        countComplete += 1;
+      }
+    }
+    dict[i] = countComplete;
+  }
+  console.log(dict);
+});
