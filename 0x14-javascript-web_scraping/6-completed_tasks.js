3b5927ecc24efe8d5bbf508445f23c86d975dd81
Brian Zapata <dzapata397@gmail.com>
2021-09-14 22:03:30 -0500

    The original code and the condition was added

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,21 +1,20 @@
 #!/usr/bin/node
 /* This script computes the number of tasks completed by user id */
 const request = require('request');
+const dict = {};
 
-request(process.argv[2], (error, process, body) => {
+request.get(process.argv[2], (error, process, body) => {
   if (error) {
     return console.error(error);
   }
-  const listTask = JSON.parse(body);
-  const dict = {};
   for (let i = 1; i < 11; i++) {
     let countComplete = 0;
-    for (const task of listTask) {
+    for (const task of JSON.parse(body)) {
       if (task.userId === i && task.completed === true) {
         countComplete += 1;
       }
     }
-    if (countComplete !== 0){
+    if (countComplete !== 0) {
       dict[i] = countComplete;
     }
   }
