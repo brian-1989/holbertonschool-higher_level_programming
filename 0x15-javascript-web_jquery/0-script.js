77013ffe6e1622194bc5c5f78d92e3a41b12c6ce
Brian Zapata <dzapata397@gmail.com>
2021-09-22 11:32:30 -0500

    The color was changed hex code

--- 0x15-javascript-web_jquery/0-script.js
+++ 0x15-javascript-web_jquery/0-script.js
@@ -1,3 +1,3 @@
 #!/usr/bin/node
 /* This script updates the text in the header tag */
-document.querySelector('header').style.color = 'red';
+document.querySelector('header').style.color = '#FF0000';
