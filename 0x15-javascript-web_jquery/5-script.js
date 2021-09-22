99ea5cc7dad2d801257934dc0054965a60cc233a
Brian Zapata <dzapata397@gmail.com>
2021-09-22 14:48:19 -0500

    The comments were added

--- 0x15-javascript-web_jquery/5-script.js
+++ 0x15-javascript-web_jquery/5-script.js
@@ -1,4 +1,5 @@
-/* This script */
+/* This script adds a li element to a list when
+the user clicks on the tag add_item*/
 $('DIV#add_item').click(function () {
 	$('ul').append("<li>Item</li>");
 });
