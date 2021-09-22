711e9e645d4e381b5e13135ec0928abaf1e6321c
Brian Zapata <dzapata397@gmail.com>
2021-09-22 17:13:12 -0500

    The comments was update

--- 0x15-javascript-web_jquery/8-script.js
+++ 0x15-javascript-web_jquery/8-script.js
@@ -1,4 +1,4 @@
-/* This script fetches and lists the title for all movies by using this URL */
+/* This script fetches and lists the title for all movies from an URL */
 $.get('https://swapi-api.hbtn.io/api/films/?format=json', function (data) {
   $.each(data.results, function (value) {
     $('UL#list_movies').append('<li>' + data.results[value].title + '</li>');
